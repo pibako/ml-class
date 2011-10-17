ea8c9855f29817ad43c620673432c5c2fd5d4c2c
Piotr Kowalski <piotrek.kowalski@gmail.com>
2011-10-18 00:44:21 +0200

    gradient decent for one variable

--- mlclass-ex1/mlclass-ex1/gradientDescent.m
+++ mlclass-ex1/mlclass-ex1/gradientDescent.m
@@ -8,7 +8,6 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
@@ -16,18 +15,26 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+  
+  h = X*theta;
 
+  delta1 = (1/m) * sum((h - y).*X(:,1));
+  delta2 = (1/m) * sum((h - y).*X(:,2));
 
+  t1 = theta(1) - alpha * delta1;
+  t2 = theta(2) - alpha * delta2;
 
+  theta(1) = t1;
+  theta(2) = t2;
 
-
-
+  % theta = theta - alpha * (1/m) * sum((h1 - y).*X(:,2));
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
+    % J_history(iter)
 end
+
 
 end
